a66fefa59172df9e7c7a9d6bc0d44775b945e0e8
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-07-22 17:33:25 -0500

    classification use case and generation

--- text_generation/text_gen.py
+++ text_generation/text_gen.py
@@ -22,5 +22,23 @@ REGION="us-central1"
 vertexai.init(project=PROJECT_ID,location=REGION,credentials=Credentials)
 
 model = GenerativeModel("gemini-2.0-flash-001")
-response = model.generate_content("Hi, what is photosynthesis.")
+
+# response = model.generate_content("Hi, what is photosynthesis.")
+# print(response.text)
+
+prompt="""
+ I want you to help me choose the best way to lose weight as an obese 45 old male.
+
+ Question:"what is the best way to lose weight"
+
+ options:
+ 1.regular diet especially salads,drink much water and exercise regularly
+ 2.Drink wine and grape related drinks
+ 3.drink plenty of alcohol
+
+
+please select one of the options and explain why is it the best
+
+"""
+response=model.generate_content(prompt)
 print(response.text)
