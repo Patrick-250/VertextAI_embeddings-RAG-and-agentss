fb799a15f126a725abe44cb963ad4032e8f7c178
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-07-22 18:30:09 -0500

    top n and top k

--- text_generation/selecting_tops.py
+++ text_generation/selecting_tops.py
@@ -5,7 +5,6 @@ from vertexai.generative_models import GenerativeModel,GenerationConfig
 key_path="../vertexai.json"
 
 Credentials=Credentials.from_service_account_file(
-
   key_path,
   scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
@@ -16,24 +15,23 @@ if Credentials.expired:
 PROJECT_ID="vertex-ai-466717"
 REGION="us-central1"
 
-
 #start of vertex ai initialization
 
 vertexai.init(project=PROJECT_ID,location=REGION,credentials=Credentials)
 
 model = GenerativeModel("gemini-2.0-flash-001")
 
-#lets control temperature(randomness and creativity)
-
-
 prompt="""
-who is the richest person in africa as of 2024
+create the best slogan for online music streaming website/app that features unique experience
 
 """
-config = GenerationConfig(temperature=1.0)
-response=model.generate_content(
-  prompt,generation_config=config
-)
+top_k = 20
+top_p = 0.7
+config = GenerationConfig(temperature=0.9, top_p=top_p, top_k=top_k)
+response = model.generate_content(prompt, generation_config=config)
+
+print(f"[top_k = {top_k}]")
+print(f"[top_p = {top_p}]")
 print(response.text)
 
-#change the temperature to 0 and see the randomness decrease
+#top n and top k are really probability controllers,,, in return help randomness etc... research.....
